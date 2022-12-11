50bb20d35a9bd6f14e226a263aa522fd1700cb6b
Konstantin Umanets <konstantin@osh4.com>
2022-12-12 01:14:24 +0400

    Fix the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,39 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
-import { ChildComponent} from './child.component';
+import {Component} from '@angular/core';
 
-class Item{
+class Item {
     purchase: string;
     done: boolean;
     price: number;
-     
+
     constructor(purchase: string, price: number) {
-  
+
         this.purchase = purchase;
         this.price = price;
         this.done = false;
     }
 }
- 
+
 @Component({
     selector: 'purchase-app',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent { 
-   @ViewChild("nameText", {static: false})
-    nameParagraph: ElementRef;
-     
-    name: string = "Tom";
-     
-    change() { 
-        console.log(this.nameParagraph.nativeElement.textContent); 
-        this.nameParagraph.nativeElement.textContent = "hell";
+export class AppComponent {
+    text: string = "";
+    price: number = 0;
+
+    items: Item[] =
+        [
+            {purchase: "Хлеб", done: false, price: 15.9},
+            {purchase: "Масло", done: false, price: 60},
+            {purchase: "Картофель", done: true, price: 22.6},
+            {purchase: "Сыр", done: false, price: 310}
+        ];
+
+    addItem(text: string, price: number): void {
+
+        if (text == null || text.trim() == "" || price == null)
+            return;
+        this.items.push(new Item(text, price));
     }
 }
